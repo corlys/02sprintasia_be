535dda612dbed19abc49a206a5f231c819f7a1a5
corlys <duscae047@gmail.com>
2024-08-21 09:10:56 +0700

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 // src/index.js
 import express, { Express } from "express";
+import cors from "cors";
 import { tasksRouter, subTasksRouter } from "./routers";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/tasks", tasksRouter);
 app.use("/subtasks", subTasksRouter);
 
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from "./app";
 
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.APP_PORT || "3000";
 
 app.listen(PORT, function () {
   console.log(`Server is runngin on port ${PORT}`);
